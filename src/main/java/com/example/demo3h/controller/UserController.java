6d57e42984b9e77a9b53992f0f5a78a1a8bdad14
Sakura3h <hellosakura457@163.com>
2018-11-29 10:44:00 +0800

    commit spring boot

--- src/main/java/com/example/demo3h/controller/UserController.java
+++ src/main/java/com/example/demo3h/controller/UserController.java
@@ -5,13 +5,16 @@ import com.example.demo3h.model.TbUserInfo;
 import com.example.demo3h.service.TbUserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
-@RestController
+@Controller
 public class UserController {
     @Autowired
     private TbUserInfoService tbUserInfoService;
@@ -28,5 +31,11 @@ public class UserController {
           r.setStatus("error");
      }
         return ResponseEntity.ok(r);
+    }
+@RequestMapping("/demo")
+    public  String demo(Model model){
+        List<TbUserInfo> list =tbUserInfoService.find();
+        model.addAttribute("list",list);
+        return  "/index";
     }
 }
