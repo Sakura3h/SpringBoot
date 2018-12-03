759d5388d7ea5c319b8ce69af4203b14f810cae7
Sakura3h <hellosakura457@163.com>
2018-12-03 13:59:26 +0800

    banner

---
+++ src/main/java/com/example/demo3h/service/Impl/TbUserInfoServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.demo3h.service.Impl;
+
+import com.example.demo3h.mapper.TbUserInfoMapper;
+import com.example.demo3h.model.TbUserInfo;
+import com.example.demo3h.service.TbUserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TbUserInfoServiceImpl implements TbUserInfoService{
+    @Autowired
+    private  TbUserInfoMapper tbUserInfoMapper;
+
+    @Override
+    public List<TbUserInfo> findAll() {
+        return tbUserInfoMapper.findAll();
+    }
+}
